9b2784b3cee1c5dc3a0678d1d884e472f8b2e269
Axelle Abbadie <axelle.abbadie@univ-montp3.fr>
2024-03-05 22:49:04 +0100

    Quartz sync: Mar 5, 2024, 10:49 PM

--- quartz_sync.sh
+++ quartz_sync.sh
@@ -1,2 +1,4 @@
 #!/bin/zsh
+export PATH="$PATH:/Users/axelleabbadie/.nvm/versions/node/v18.14.2/bin"
+cd "/Users/axelleabbadie/SynologyDrive/Professionnel/Salarié/HumaNum/Prison & éducation/Chapitre 3/ch3_forfert"
 npx quartz sync --no-pull
